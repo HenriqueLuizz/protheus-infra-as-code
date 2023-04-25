a098f699619c3edba5bd9316cd97c27d99724079
Henrique Luiz <henriqueluiz_silva@yahoo.com.br>
2023-04-25 14:33:45 -0300

    fix: removido variavel private_key_path não usada

--- provider.simple.tf
+++ provider.simple.tf
@@ -10,7 +10,6 @@ provider "oci" {
 variable tenancy_ocid {}
 variable user_ocid {}
 variable fingerprint {}
-variable private_key_path {}
 variable private_key {}
 variable compartment_ocid {}
 variable region {}
